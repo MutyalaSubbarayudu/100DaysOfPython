e50623c2ef4f5dedbc2f661048a352ee2f6796da
Mutyala Subbarayudu <mutyalasubbu95@gmail.com>
2021-03-13 15:31:43 +0530

    Updated Comments in print function file

--- Day 1/print function.py
+++ Day 1/print function.py
@@ -1,2 +1,4 @@
-# print function will print the messsage passed in ""/'' and if nested function is there it will process and then print it 
+# print function -- syntax: print()
+# 1. will print the messsage passed in ""/'' 
+# 2. if nested function is there it will process and then print it 
 print("Hello World")
