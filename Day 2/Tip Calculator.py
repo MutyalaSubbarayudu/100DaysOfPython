3e533d0d4bc4af94ff7a78e6cb93764c7e469a74
Mutyala Subbarayudu <mutyalasubbu95@gmail.com>
2021-03-14 21:15:08 +0530

    Code updated with Format function

--- Day 2/Tip Calculator.py
+++ Day 2/Tip Calculator.py
@@ -6,6 +6,11 @@ number_of_people = int( input("How many people to split the bill? ") )
 
 total_bill += total_bill * ( tip_percentage / 100 )
 
-individual_bill_amount = round( total_bill / number_of_people , 2 )
+# individual_bill_amount = round( total_bill / number_of_people , 2 )
+
+individual_bill_amount = total_bill/number_of_people
+
+# https://stackoverflow.com/questions/455612/limiting-floats-to-two-decimal-points
+individual_bill_amount = "{:.2f}".format(individual_bill_amount)
 
 print(f"Each person should pay: ${ individual_bill_amount }")
